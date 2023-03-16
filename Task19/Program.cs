f31a5aa578056674c94afaa8c837486d2696a717
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-16 13:19:23 +0300

    Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,11 +6,11 @@ if(len == 5)
 {
     if(number[0] == number[4] && number[1] == number [3])
     {
-        Console.WriteLine(${number} - палиндром);
+        Console.WriteLine($"{number} - палиндром");
     }
     else
     {
-        Console.WriteLine(${number} - не палиндром);
+        Console.WriteLine($"{number} - не палиндром");
     }
 }
 else
